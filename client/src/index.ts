7c2ba54d86657f97f9f3adae93adffcf7ed02995
Hyeon Kim <simnalamburt@gmail.com>
2020-03-20 12:06:32 +0900

    Run prettier

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,35 +3,29 @@ import Vue from 'vue'
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
-  }
+    message: 'Hello Vue!',
+  },
 })
 setTimeout(_ => {
   app.message += 'ㅇㅅㅇ~'
 }, 1000)
 
-
-
 new Vue({
   el: '#app-2',
   data: {
-    message: 'You loaded this page on ' + new Date()
-  }
+    message: 'You loaded this page on ' + new Date(),
+  },
 })
-
-
 
 const app3 = new Vue({
   el: '#app-3',
   data: {
-    seen: false
-  }
+    seen: false,
+  },
 })
 setTimeout(_ => {
   app3.seen = true
 }, 500)
-
-
 
 new Vue({
   el: '#app-4',
@@ -39,40 +33,37 @@ new Vue({
     todos: [
       { text: 'Learn JavaScript' },
       { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
-  }
+      { text: 'Build something awesome' },
+    ],
+  },
 })
-
-
 
 new Vue({
   el: '#app-5',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
   },
   methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
+    reverseMessage: function() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('')
+    },
+  },
 })
-
-
 
 new Vue({
   el: '#app-6',
   data: {
-    message: 'Hello Vue!'
-  }
+    message: 'Hello Vue!',
+  },
 })
-
-
 
 // Define a new component called todo-item
 Vue.component('todo-item', {
-  props: ['todo']
-  template: '<li>{{ todo.text }}</li>'
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>',
 })
 var app7 = new Vue({
   el: '#app-7',
@@ -80,7 +71,7 @@ var app7 = new Vue({
     groceryList: [
       { id: 0, text: 'Vegetables' },
       { id: 1, text: 'Cheese' },
-      { id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
-  }
+      { id: 2, text: 'Whatever else humans are supposed to eat' },
+    ],
+  },
 })
