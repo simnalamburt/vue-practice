00144ae1dcb1d39f428a2a1e9ff2875787efa101
Hyeon Kim <simnalamburt@gmail.com>
2021-05-11 13:51:20 +0900

    Use only one root

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <practice-1/>
+  <hr/>
+  <practice-2/>
+  <hr/>
+  <practice-3/>
+  <hr/>
+  <practice-4/>
+  <hr/>
+  <practice-5/>
+  <hr/>
+  <practice-6/>
+  <hr/>
+  <practice-7/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Practice1 from './Practice1.vue'
+import Practice2 from './Practice2.vue'
+import Practice3 from './Practice3.vue'
+import Practice4 from './Practice4.vue'
+import Practice5 from './Practice5.vue'
+import Practice6 from './Practice6.vue'
+import Practice7 from './Practice7.vue'
+
+export default defineComponent({
+  components: {
+    Practice1,
+    Practice2,
+    Practice3,
+    Practice4,
+    Practice5,
+    Practice6,
+    Practice7,
+  },
+})
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
 import { createApp } from 'vue'
-import App1 from './App1.vue'
-import App2 from './App2.vue'
-import App3 from './App3.vue'
-import App4 from './App4.vue'
-import App5 from './App5.vue'
-import App6 from './App6.vue'
-import App7 from './App7.vue'
+import App from './App.vue'
 
-createApp(App1).mount('#app-1')
-createApp(App2).mount('#app-2')
-createApp(App3).mount('#app-3')
-createApp(App4).mount('#app-4')
-createApp(App5).mount('#app-5')
-createApp(App6).mount('#app-6')
-createApp(App7).mount('#app-7')
+createApp(App).mount('#app')
